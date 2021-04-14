9cca0503babf8b85f88b1681875ce842c6e4b5dc
Juan Antonio Breña Moral <bren@juanantonio.info>
2021-04-14 11:11:19 +0200

    Example for One to One

--- tema-10/spring-data-jdbc-basics/onetoone/src/test/java/info/jab/microservices/repository/OneToOneRepositoryTest.java
+++ tema-10/spring-data-jdbc-basics/onetoone/src/test/java/info/jab/microservices/repository/OneToOneRepositoryTest.java
@@ -3,6 +3,7 @@ package info.jab.microservices.repository;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Date;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,18 +23,46 @@ public class OneToOneRepositoryTest {
 	@Test
 	@DisplayName("One-to-One Mapping Test")
 	void embeddedMappingTest() {
+
 		final Credentials credentials = new Credentials();
 		credentials.setUserName("peterm");
 		credentials.setPassword("password");
+
 		final User user = new User();
 		user.setCreatedTime(new Date());
 		user.setDateofBirth(new Date());
 		user.setUserType(UserType.EMPLOYEE);
 		user.setCredentials(credentials);
+
 		final User createdUser = userRepository.save(user);
 		assertTrue(createdUser != null);
+
 		userRepository.delete(user);
 		assertTrue(userRepository.findByUserName(user.getCredentials().getUserName()) == 0);
+	}
+
+	@Test
+	@DisplayName("One-to-One Mapping Test")
+	void embeddedMappingTest2() {
+
+		final Credentials credentials = new Credentials();
+		credentials.setUserName("peterm");
+		credentials.setPassword("password");
+
+		final User user = new User();
+		user.setCreatedTime(new Date());
+		user.setDateofBirth(new Date());
+		user.setUserType(UserType.EMPLOYEE);
+		user.setCredentials(credentials);
+
+		final User createdUser = userRepository.save(user);
+		assertTrue(createdUser != null);
+
+		createdUser.setCredentials(null);
+		userRepository.save(createdUser);
+
+		StreamSupport.stream(userRepository.findAll().spliterator(), false)
+				.forEach(x -> x.toString());
 	}
 
 }
